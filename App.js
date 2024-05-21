b971efb581e041f72ef683824c68dbe8e2d2c39f
Muhammad Sameer <136972597+Sameer-khan0@users.noreply.github.com>
2024-05-21 13:13:57 +0500

    React Native Setup using expo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Fuck you world</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,6 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    fontSize: '40px',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
